d819193eaf24f714d4aa0697c6c2b32b04ed4c9b
Amber <ambermcknight54@yahoo.com>
2026-06-17 22:12:52 -0500

    level 4 complete

--- lesson-01-nodejs/level-04-error-handling/script.js
+++ lesson-01-nodejs/level-04-error-handling/script.js
@@ -1,1 +1,18 @@
 console.log("Attempting to access browser features...");
+try {
+  localStorage["username"] = "Amber";
+  const paragraph = document.querySelector("p");
+
+  console.log("Success!");
+} catch (error) {
+  // Tasks 5 & 6: Catching the error and explaining why it happened
+  console.log("⚠️ An Error Was Caught Safely!");
+  console.log("Reason for error:", error.message);
+  console.log("\nExplanation:");
+  console.log(
+    "This error occurs because we are running this script inside Node.js.",
+  );
+  console.log(
+    "Node.js objects like 'localStorage' and 'document' do not exist here!",
+  );
+}
