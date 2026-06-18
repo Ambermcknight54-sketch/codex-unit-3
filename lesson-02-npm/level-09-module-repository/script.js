a125c04bf8ea4468831d60ed9fb122fcd3e683cd
Amber <ambermcknight54@yahoo.com>
2026-06-18 17:51:16 -0500

    ran terminal commands

---
+++ lesson-02-npm/level-09-module-repository/script.js
@@ -0,0 +1,12 @@
+// import figlet from "figlet";
+
+// // Tasks 7 & 8: Use figlet to convert your text into a large ASCII art banner
+// figlet("Hello Figlet!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   // Print the beautiful banner message
+//   console.log(data);
+// });
