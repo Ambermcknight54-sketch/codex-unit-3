f153a445153ed8449c443ab36dfc487df181a898
Amber <ambermcknight54@yahoo.com>
2026-06-17 21:34:24 -0500

    level 1 complete

---
+++ lesson-01-nodejs/level-02-nodejs-script/script.js
@@ -0,0 +1,8 @@
+console.log("To run a JavaScript file in your terminal, follow these steps:");
+console.log(
+  "1. Open your terminal (like Git Bash or the built-in VS Code terminal).",
+);
+console.log(
+  "2. Use the 'cd' command followed by the folder path to navigate to where this file is saved.",
+);
+console.log("3. Type 'node script' (or 'node script.js') and press Enter!");
