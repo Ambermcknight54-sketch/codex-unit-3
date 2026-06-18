3a614cfcc7c0ff53ef89800bf97ad986d04caa99
Amber <ambermcknight54@yahoo.com>
2026-06-17 21:40:53 -0500

    level 4 complete

--- lesson-02-npm/level-04-node-modules/script.js
+++ lesson-02-npm/level-04-node-modules/script.js
@@ -1,1 +1,25 @@
-import {} from "node:os";
+import { cpus, totalmem, freemem, machine, platform } from "node:os";
+
+const cpuInfo = cpus();
+const totalmemInfo = totalmem();
+const freememInfo = freemem();
+const machineInfo = machine();
+const platformInfo = platform();
+
+console.log("CPU:", cpuInfo);
+console.log("Total Memory:", totalmemInfo);
+console.log("Free Memory:", freememInfo);
+console.log("Machine:", machineInfo);
+console.log("Platform:", platformInfo);
+
+// const cpuInfo = cpus();
+// const totalmemInfo = totalmem();
+// const freememInfo = freemem();
+// const machineInfo = machine();
+// const platformInfo = platform();
+
+// console.log("CPU:", (cpuInfo);
+// console.log(totalmemInfo);
+// console.log(freememInfo);
+// console.log(machineInfo);
+// console.log(platformInfo);
