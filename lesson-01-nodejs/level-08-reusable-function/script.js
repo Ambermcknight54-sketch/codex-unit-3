2066126072acbaed6a096de33000789636e13f06
Amber <ambermcknight54@yahoo.com>
2026-06-17 22:48:58 -0500

    level 8 complete

---
+++ lesson-01-nodejs/level-08-reusable-function/script.js
@@ -0,0 +1,19 @@
+// 2. Create an array with 10 values
+const fruitItems = [
+  "Apples",
+  "Bananas",
+  "Cherries",
+  "Dates",
+  "Elderberries",
+  "Figs",
+  "Grapes",
+  "Honeydew",
+  "Kiwi",
+  "Lemon",
+];
+
+// 3, 4, 5. Loop to traverse the array and display numbered 1 to 10
+for (let i = 0; i < fruitItemsItems.length; i++) {
+  // Adding 1 to 'i' so the display reads 1-10 instead of 0-9
+  console.log(`${i + 1}. ${fruitItems[i]}`);
+}
