1b085eea63285ae3fb39af918c03963f9b97e2ce
Amber <ambermcknight54@yahoo.com>
2026-06-17 21:35:55 -0500

    level 7 complete

---
+++ lesson-01-nodejs/level-07-array-traversal/script.js
@@ -0,0 +1,6 @@
+const numbers = ["11", "22", "33", "44"];
+for (let index = 0; index < numbers.length; index++) {
+  const item = numbers[index];
+  const itemNumber = index + 1;
+  console.log(`${itemNumber}. ${item}`);
+}
