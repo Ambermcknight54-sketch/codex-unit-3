24a0c3611bc4f590c2649e655a0489fa3fdd60b7
Amber <ambermcknight54@yahoo.com>
2026-06-17 21:38:46 -0500

    level 1 complete

--- lesson-02-npm/level-01-exports-imports/index.js
+++ lesson-02-npm/level-01-exports-imports/index.js
@@ -6,3 +6,9 @@ import names from "./names.js";
 greet("Frontend Developer");
 stats(3, 7);
 showNames(names);
+
+console.log("Benefits of Exports & Imports");
+console.log("Using export and import makes code modular. This helps by:");
+console.log("- Keeping code organized and readable.");
+console.log("- Making components easier to debug.");
+console.log("- Allows code to be reused.");
