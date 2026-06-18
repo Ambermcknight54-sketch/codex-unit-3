8892d2fc7b96cd02b88415dafc8dd85e9d34d152
Amber <ambermcknight54@yahoo.com>
2026-06-17 22:07:32 -0500

    level 3 complete

--- lesson-01-nodejs/level-03-no-dom/script.js
+++ lesson-01-nodejs/level-03-no-dom/script.js
@@ -1,3 +1,5 @@
 console.log("Attempting to access browser features...");
 localStorage.setItem("username", "Amber");
 const paragraph = document.querySelector("p");
+localStorage.setItem("username", "Amber");
+const user = localStorage.getItem("username");
