92bbde5fa13d6bc9a8a1d765b0c74fbe9a293c28
Amber <ambermcknight54@yahoo.com>
2026-06-18 16:26:27 -0500

    ran java script

--- lesson-02-npm/level-02-default-imports/definedMessage.js
+++ lesson-02-npm/level-02-default-imports/definedMessage.js
@@ -1,1 +1,2 @@
-const definedMessage = "";
+const definedMessage =
+  "To use 'export default', you can define a variable, function, or class first, and then export it at the bottom of the file using the syntax: export default variableName;";
--- lesson-02-npm/level-02-default-imports/directMessage.js
+++ lesson-02-npm/level-02-default-imports/directMessage.js
@@ -1,1 +1,1 @@
-export default "";
+export default "Default imports allow you to import a default export using any variable name you want. You don't have to match the original name in the source file!";
