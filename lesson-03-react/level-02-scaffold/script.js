52d4d835529fd97dbe990220ae2cc5e6fbcdcc7c
Amber <ambermcknight54@yahoo.com>
2026-06-19 15:20:04 -0500

    added script.js  and console.log

---
+++ lesson-03-react/level-02-scaffold/script.js
@@ -0,0 +1,4 @@
+console.log("Scaffolding means setting up the basic structure of a project.");
+console.log(
+  "-creating the essential files and folders (like index.html, style.css, and script.js)",
+);
