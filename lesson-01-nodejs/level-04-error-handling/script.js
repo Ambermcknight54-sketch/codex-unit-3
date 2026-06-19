41b645677ecf17fd7e76b47588392414c2506c81
Amber <ambermcknight54@yahoo.com>
2026-06-18 22:44:23 -0500

    level 4 complete

--- lesson-01-nodejs/level-04-error-handling/script.js
+++ lesson-01-nodejs/level-04-error-handling/script.js
@@ -1,19 +1,11 @@
 console.log("Attempting to access browser features...");
 localStorage.setItem("username", "Amber");
-const pTag = document.querySelector("p");
+const pTag = document.querySelector("pTag");
 
 try {
   localStorage.setItem("username", "Amber");
-  const pTag = document.querySelector("p");
-
+  const pTag = document.querySelector("pTag");
   console.log("Success!");
 } catch (error) {
-  // Tasks 5 & 6: Catching the error and explaining why it happened
-  console.log("The is an error!");
-  console.log("Reason for error:", error.message);
-  console.log("\n local storage not provided");
-  console.log(
-    "This error occurs because we are running this script inside Node.js.",
-  );
-  console.log("Node.js objects like 'localStorage' do not exist here!");
+  console.log("This is an error!");
 }
