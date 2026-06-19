db09d2dd985c436858f721192e30b6ae06fa6d3d
Amber <ambermcknight54@yahoo.com>
2026-06-18 19:49:57 -0500

    commented out starter

--- lesson-01-nodejs/level-03-no-dom/script.js
+++ lesson-01-nodejs/level-03-no-dom/script.js
@@ -1,10 +1,29 @@
 console.log("Attempting to access browser features...");
-localStorage.setItem("username", "Amber");
-const pTag = document.querySelector("p");
 
 try {
-  console.log("Attempting to access browser features...");
+  // Move these inside the try block so the catch block can actually handle the error
   localStorage.setItem("username", "Amber");
+  const pTag = document.querySelector("p");
+
+  console.log("Success!");
 } catch (error) {
-  console.log("There is an error.");
+  // Tasks 5 & 6: Catching the error and explaining why it happened
+  console.log("There is an error!");
+  console.log("Reason for error:", error.message);
+  console.log("\nLocal storage or DOM features are not available.");
+  console.log(
+    "This error occurs because we are running this script inside Node.js.",
+  );
+  console.log(" localStorage and document do not exist here!");
 }
+
+// console.log("Attempting to access browser features...");
+// localStorage.setItem("username", "Amber");
+// const pTag = document.querySelector("p");
+
+// try {
+//   console.log("Attempting to access browser features...");
+//   localStorage.setItem("username", "Amber");
+// } catch (error) {
+//   console.log("There is an error.");
+// }
