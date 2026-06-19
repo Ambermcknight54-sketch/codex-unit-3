95f06482b2afbf6eee7fee90474125a479c0e19c
Amber <ambermcknight54@yahoo.com>
2026-06-19 16:40:50 -0500

    level 2 complete

--- lesson-03-react/level-02-scaffold/script.js
+++
@@ -1,4 +0,0 @@
-console.log("Scaffolding means setting up the basic structure of a project.");
-console.log(
-  "-creating the essential files and folders (like index.html, style.css, and script.js)",
-);
