89b745510e6e9fa0b008af24095ee2dc20c00df2
Amber <ambermcknight54@yahoo.com>
2026-06-19 19:27:26 -0500

    level 9 changed script

--- lesson-02-npm/level-09-module-repository/script.js
+++ lesson-02-npm/level-09-module-repository/script.js
@@ -1,12 +1,12 @@
-// import figlet from "figlet";
+import figlet from "figlet";
 
-// // Tasks 7 & 8: Use figlet to convert your text into a large ASCII art banner
-// figlet("Hello Figlet!", function (err, data) {
-//   if (err) {
-//     console.log("Something went wrong...");
-//     console.dir(err);
-//     return;
-//   }
-//   // Print the beautiful banner message
-//   console.log(data);
-// });
+// Tasks 7 & 8: Use figlet to convert your text into a large ASCII art banner
+figlet("Hello Figlet!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  // Print the beautiful banner message
+  console.log(data);
+});
