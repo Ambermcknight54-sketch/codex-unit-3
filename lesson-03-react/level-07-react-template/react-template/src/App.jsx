69bdce357428cfc17d39471c678a817074676be9
Amber <ambermcknight54@yahoo.com>
2026-06-22 22:53:46 -0500

    created react template

---
+++ lesson-03-react/level-07-react-template/react-template/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  <main>
+    <h1>React Template</h1>
+    <p>
+      To build a React template from scratch, use the terminal command "npm
+      create vite@latest set a folder-name to scaffold the files. Next, navigate
+      into that new folder using "cd folder-name" and run "npm install" to
+      download the core React packages. Finally, clear out the dcode and styling
+      inside the src folder that is a blank, and start the local environment
+      using "npm run dev".
+    </p>
+  </main>;
+}
+
+export default App;
