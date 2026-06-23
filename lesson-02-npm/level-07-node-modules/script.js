ab0d4c4751db7a8e1838f00a9e3815086203c038
Amber <ambermcknight54@yahoo.com>
2026-06-22 19:38:08 -0500

    added more discription of chalk

--- lesson-02-npm/level-07-node-modules/script.js
+++ lesson-02-npm/level-07-node-modules/script.js
@@ -1,5 +1,7 @@
 // const message = "chalk. "
 import chalk from "chalk";
 console.log(
-  chalk.green("A folder that contains external modules or libraries."),
+  chalk.green(
+    "A folder that contains external modules or libraries and explains what they are.",
+  ),
 );
