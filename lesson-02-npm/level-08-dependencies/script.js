76983cafa72daffe1951ee8324a432068179c5b9
Amber <ambermcknight54@yahoo.com>
2026-06-24 22:08:01 -0500

    addded what dependencies are

--- lesson-02-npm/level-08-dependencies/script.js
+++ lesson-02-npm/level-08-dependencies/script.js
@@ -1,3 +1,6 @@
 import chalk from "chalk";
 
 console.log(chalk.green("Success! Chalk is installed and working."));
+console.log(
+  "dependencies are external code libraries, tools, or packages that your project relies on to function properly.",
+);
