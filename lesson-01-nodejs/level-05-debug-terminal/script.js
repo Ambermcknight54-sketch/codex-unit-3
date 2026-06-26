818552dc7734a64c21a9e4a9509952550e58cfb1
Amber <ambermcknight54@yahoo.com>
2026-06-25 21:57:16 -0500

    removed stray }, practicing for interview

--- lesson-01-nodejs/level-05-debug-terminal/script.js
+++ lesson-01-nodejs/level-05-debug-terminal/script.js
@@ -2,20 +2,19 @@ console.log("Roses are red.");
 console.log("Violets are blue.");
 
 try {
-  // Interacting with browser features using standard bracket notation (no dots)
-  localStorage["username"] = "Amber";
-  const paragraph = document["querySelector"]("p");
+  localStorage.username = "Amber";
+  const paragraph = document.querySelector("p");
 } catch (error) {
   console.log("An error was caught");
-
-  // Task 6 & 7: Message explaining how to use the JavaScript Debug Terminal
-  console.log("\n How to use the JavaScript Debug Terminal");
-  console.log("1. Open the terminal panel in VS Code (Ctrl+` or Cmd+`).");
-  console.log(
-    "2. Click the dropdown arrow next to the '+' icon in the terminal window panel.",
-  );
-  console.log("3. Select 'JavaScript Debug Terminal' from the menu.");
-  console.log(
-    "4. Navigate to your folder and type 'node script'—it will automatically trigger any breakpoints!",
-  );
 }
+
+// Task 6 & 7: Message explaining how to use the JavaScript Debug Terminal
+console.log("\n How to use the JavaScript Debug Terminal");
+console.log("1. Open the terminal panel in VS Code (Ctrl+` or Cmd+`).");
+console.log(
+  "2. Click the dropdown arrow next to the '+' icon in the terminal window panel.",
+);
+console.log("3. Select 'JavaScript Debug Terminal' from the menu.");
+console.log(
+  "4. Navigate to your folder and type 'node script'—it will automatically trigger any breakpoints!",
+);
