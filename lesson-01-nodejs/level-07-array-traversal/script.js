03e54bc5f8712b62b453a66600f90f2b7eae4ffd
Amber <ambermcknight54@yahoo.com>
2026-06-26 16:14:12 -0500

    arrary has 10 items

--- lesson-01-nodejs/level-07-array-traversal/script.js
+++ lesson-01-nodejs/level-07-array-traversal/script.js
@@ -1,4 +1,4 @@
-const numbers = ["11", "22", "33", "44"];
+const numbers = ["11", "22", "33", "44", "55", "66", "77", "88", "99", "109"];
 for (let index = 0; index < numbers.length; index++) {
   const item = numbers[index];
   const itemNumber = index + 1;
