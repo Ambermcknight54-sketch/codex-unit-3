d6705dec9dd073d9791111c7c7b342bf332f8528
Amber <ambermcknight54@yahoo.com>
2026-06-25 19:06:01 -0500

    finished level 1

---
+++ lesson-04-components/level-01-global-packages/script.js
@@ -0,0 +1,3 @@
+console.log(
+  "To install a package globally so it can be accessed from any project on your computer, run the command 'npm install -g' followed by the package name but for MAC must use hebrew",
+);
