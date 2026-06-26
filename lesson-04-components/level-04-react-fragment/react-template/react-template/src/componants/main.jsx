0c998c339faadb20b5d715896cbc366a7fbd265d
Amber <ambermcknight54@yahoo.com>
2026-06-25 21:09:59 -0500

    deleted extra react

--- lesson-04-components/level-04-react-fragment/react-template/react-template/src/componants/main.jsx
+++ lesson-04-components/level-04-react-fragment/react-template/react-template/src/componants/main.jsx
@@ -8,3 +8,19 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>,
 );
+import "react";
+
+export default function Main() {
+  return (
+    <main className="main-content">
+      <p>
+        To convert a standard layout section into a React component, you extract
+        its HTML structure and wrap it inside a JavaScript function that returns
+        that code. By separating your site into standalone components like
+        Headers, Mains, and Footers, you make your project much cleaner, easier
+        to organize, and simpler to update without scrolling through one giant
+        file.
+      </p>
+    </main>
+  );
+}
