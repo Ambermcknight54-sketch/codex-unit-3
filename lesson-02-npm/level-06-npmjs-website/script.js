761d34eb0e4fae7ba6a11252de3d160649dc08af
Amber <ambermcknight54@yahoo.com>
2026-06-26 16:27:44 -0500

    added year published

--- lesson-02-npm/level-06-npmjs-website/script.js
+++ lesson-02-npm/level-06-npmjs-website/script.js
@@ -1,17 +1,12 @@
 // Task 4-6: Create an array of objects ("things") with corrected category spellings
 // const messsage = ["website gives an array of things like catagory and publish date."]
-
 const message = [
   {
     category: "Animals & Fun",
-    explanation:
-      "The NPMJS website where you can find fun utility packages like 'cat-ascii-faces'.",
+    yearPublished: 2014,
   },
   {
     category: "Terminal Art",
-    explanation:
-      "Another interesting cat-themed package on NPMJS is 'cat-me', which prints ASCII cat art into your command line.",
+    yearPublished: 2015,
   },
 ];
-
-console.log(message);
