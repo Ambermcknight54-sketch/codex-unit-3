6eae304073093668aebac0bfef8876121b4b04d9
Amber <ambermcknight54@yahoo.com>
2026-06-26 16:35:32 -0500

    changed level 8 to cyan and kept 7 green

--- lesson-02-npm/level-08-dependencies/script.js
+++ lesson-02-npm/level-08-dependencies/script.js
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 
-console.log(chalk.green("Success! Chalk is installed and working."));
+console.log(chalk.cyan("Success! Chalk is installed and working."));
 console.log(
   "dependencies are external code libraries, tools, or packages that your project relies on to function properly.",
 );
