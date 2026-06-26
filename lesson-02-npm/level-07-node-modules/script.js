bd9870d2076afc8145a3b046f6306fd038eb28b2
Amber <ambermcknight54@yahoo.com>
2026-06-26 16:33:06 -0500

    put code in correct folder delected extra level 7

--- lesson-02-npm/level-07-node-modules/script.js
+++
@@ -1,7 +0,0 @@
-// const message = "chalk. "
-import chalk from "chalk";
-console.log(
-  chalk.green(
-    "A folder that contains external modules or libraries and explains what they are.",
-  ),
-);
---
+++ lesson-02-npm/level-07-npm-packages/script.js
@@ -0,0 +1,7 @@
+// const message = "chalk. "
+import chalk from "chalk";
+console.log(
+  chalk.green(
+    "A folder that contains external modules or libraries and explains what they are.",
+  ),
+);
