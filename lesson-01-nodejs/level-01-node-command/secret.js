1447fa525f4afc3dbd18272da0e9435d5f63e780
Amber <ambermcknight54@yahoo.com>
2026-06-26 16:10:17 -0500

    updated code

--- lesson-01-nodejs/level-01-node-command/secret.js
+++ lesson-01-nodejs/level-01-node-command/secret.js
@@ -1,1 +1,7 @@
-export default "You ran JavaScript without a browser!";
+const npmjsInfo = [
+  "NPMJS stands for Node Package Manager JavaScript. Its initial stable version was published back in 2014.",
+];
+
+console.log(npmjsInfo);
+
+export default npmjsInfo;
