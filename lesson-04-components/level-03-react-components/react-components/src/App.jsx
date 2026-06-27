2b15e713d7bd3b082c9d77d647829becd7fe78b2
Amber <ambermcknight54@yahoo.com>
2026-06-27 12:26:34 -0500

    deleted duplicates added style

--- lesson-04-components/level-03-react-components/react-components/src/App.jsx
+++ lesson-04-components/level-03-react-components/react-components/src/App.jsx
@@ -3,18 +3,17 @@
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
 import "./App.css";
+// 11. Import Title and Info from the components directory
+import { Title } from "./components/Title";
+import { Info } from "./components/Info";
 
 function App() {
+  // 12. Let the App function return a main tag
   return (
     <main>
-      <h1>React Template</h1>
-      <p>
-        To make a React template, run the Vite command in your terminal to
-        create the project. Go into the new folder and run npm install to get
-        the files ready. Then, delete the starter code and extra CSS so you have
-        a clean folder for your next project. designing your own layouts right
-        away.
-      </p>
+      {/* 13. Add the Title and Info components inside the main tag */}
+      <Title />
+      <Info />
     </main>
   );
 }
