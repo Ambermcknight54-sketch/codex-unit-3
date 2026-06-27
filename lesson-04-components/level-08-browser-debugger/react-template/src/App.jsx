1ef2624466d3683c1c3aeb4b0e1162fc6cb62219
Amber <ambermcknight54@yahoo.com>
2026-06-27 14:30:44 -0500

    level 8 added template

---
+++ lesson-04-components/level-08-browser-debugger/react-template/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+// import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>React Template</h1>
+      <p>
+        To make a React template, run the Vite command in your terminal to
+        create the project. Go into the new folder and run npm install to get
+        the files ready. Then, delete the starter code and extra CSS so you have
+        a clean folder for your next project. designing your own layouts right
+        away.
+      </p>
+    </main>
+  );
+}
+
+export default App;
