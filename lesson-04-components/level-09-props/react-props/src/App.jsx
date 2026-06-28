bb69115a128328c14b57cb133f6762551be8031f
Amber <ambermcknight54@yahoo.com>
2026-06-28 06:32:36 -0500

    added a props object to footer

--- lesson-04-components/level-09-props/react-props/src/App.jsx
+++ lesson-04-components/level-09-props/react-props/src/App.jsx
@@ -1,15 +1,9 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
-function App() {
+function Footer(props) {
   return (
-    <>
-      <Header title="React Props" />
-      <Main message="To use props in React, add custom attributes to the component tag when rendering it in the parent file. Then, accept the 'props' object as a parameter in the child component function and reference those attributes using dot notation inside curly braces." />
-      <Footer />
-    </>
+    <footer>
+      <p>{props.year} Amber McKnight 2026. .</p>
+    </footer>
   );
 }
 
-export default App;
+export default Footer;
