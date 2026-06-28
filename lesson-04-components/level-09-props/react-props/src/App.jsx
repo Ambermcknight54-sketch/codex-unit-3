9a226ecb6ab9ac9703a921721476a65fcdcb1836
Amber <ambermcknight54@yahoo.com>
2026-06-28 06:07:01 -0500

    added code to app.jsx

--- lesson-04-components/level-09-props/react-props/src/App.jsx
+++ lesson-04-components/level-09-props/react-props/src/App.jsx
@@ -1,21 +1,14 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "./assets/vite.svg";
-// import heroImg from "./assets/hero.png";
-import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <main>
-      <h1>React Template</h1>
-      <p>
-        To make a React template, run the Vite command in your terminal to
-        create the project. Go into the new folder and run npm install to get
-        the files ready. Then, delete the starter code and extra CSS so you have
-        a clean folder for your next project. designing your own layouts right
-        away.
-      </p>
-    </main>
+    <>
+      <Header title="React Props" />
+      <Main message="To use props in React, add custom attributes to the component tag when rendering it in the parent file. Then, accept the 'props' object as a parameter in the child component function and reference those attributes using dot notation inside curly braces." />
+      <Footer />
+    </>
   );
 }
 
