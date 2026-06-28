0102326e22fb369a2e74d38c80a24d1eb9cf9591
Amber <ambermcknight54@yahoo.com>
2026-06-28 06:37:30 -0500

    added my name

--- lesson-04-components/level-09-props/react-props/src/App.jsx
+++ lesson-04-components/level-09-props/react-props/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <Header title="React Props" />
       <Main message="To use props in React, add custom attributes to the component tag when rendering it in the parent file. Then, accept the 'props' object as a parameter in the child component function and reference those attributes using dot notation inside curly braces." />
-      <Footer year="2026" />
+      <Footer year="Amber Mcknight 2026" />
     </>
   );
 }
