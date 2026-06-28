6db3745410640a0706959b40291c10f996ced305
Amber <ambermcknight54@yahoo.com>
2026-06-28 05:49:25 -0500

    added code to main

--- lesson-04-components/level-08-browser-debugger/react-debugger/src/main.jsx
+++ lesson-04-components/level-08-browser-debugger/react-debugger/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+function Main() {
+  const message =
+    "To start the debugger for React projects: launch your Vite dev server in the terminal, open the VS Code Run and Debug panel, select your custom 'React Debugger' launch configuration, and click the play button to launch the browser connection.";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  return (
+    <main>
+      <h2>Application Workspace</h2>
+      <p>{message}</p>
+    </main>
+  );
+}
+
+export default Main;
