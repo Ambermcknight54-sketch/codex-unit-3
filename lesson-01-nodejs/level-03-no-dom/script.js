bde9b37905fc65b61a615c3f7deea5c2ef16cdaf
Amber <ambermcknight54@yahoo.com>
2026-06-29 18:31:08 -0500

    commeted out what i dont need for badge interview

--- lesson-01-nodejs/level-03-no-dom/script.js
+++ lesson-01-nodejs/level-03-no-dom/script.js
@@ -3,17 +3,17 @@ console.log("Attempting to access browser features...");
 try {
   // Move these inside the try block so the catch block can actually handle the error
   localStorage.setItem("username", "Amber");
-  const pTag = document.querySelector("p");
+  const pTag = document.querySelector("pTag");
 
   console.log("Success!");
 } catch (error) {
   // Tasks 5 & 6: Catching the error and explaining why it happened
   console.log("There is an error!");
-  console.log("Reason for error:", error.message);
+  // console.log("Reason for error:", error.message);
   console.log("\nLocal storage or DOM features are not available.");
-  console.log(
-    "This error occurs because we are running this script inside Node.js.",
-  );
+  // console.log(
+  // //   "This error occurs because we are running this script inside Node.js.",
+  // );
   console.log(" localStorage and document do not exist here!");
 }
 
