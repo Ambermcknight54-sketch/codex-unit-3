34885090e1ee7e0b4f13c770f24b90b49dc52fee
Amber <ambermcknight54@yahoo.com>
2026-06-29 07:18:56 -0500

    created script.js and import

---
+++ lesson-05-react-state/level-01-object-destructuring/script.js
@@ -0,0 +1,8 @@
+import { props } from "./props.js";
+
+// Unpacking properties directly into standalone variables
+const { title, message } = props;
+
+// Displaying the values of the newly created variables
+console.log(title);
+console.log(message);
