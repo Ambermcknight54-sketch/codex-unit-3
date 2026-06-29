59c41bbc2f428b57a02624368a0e3dd57ba05dd1
Amber <ambermcknight54@yahoo.com>
2026-06-29 09:01:22 -0500

    added code

---
+++ lesson-04-components/level-05-section-components/react-sections/components/main.jsx
@@ -0,0 +1,14 @@
+function Main() {
+  return (
+    <main>
+      <p>
+        Instead of keeping all your HTML in one giant file, you can cut it into
+        smaller pieces called components. Put the header, main content, or
+        footer—into its own separate file. This keeps your project neat,
+        organized, and very easy to fix.
+      </p>
+    </main>
+  );
+}
+
+export default Main;
