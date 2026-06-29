a94352f094c1460c8be1694b4d9cb3e555b63a66
Amber <ambermcknight54@yahoo.com>
2026-06-29 07:17:09 -0500

    added props.js and export

---
+++ lesson-05-react-state/level-01-object-destructuring/props.js
@@ -0,0 +1,5 @@
+export const props = {
+  title: "Object Destructuring",
+  message:
+    "Object destructuring is a JavaScript expression that allows you to unpack properties from an object directly into distinct variables using a single line of code, matching the variable names to the object keys.",
+};
