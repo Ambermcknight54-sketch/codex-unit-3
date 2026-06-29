53d90f29b6c1ab12b70c6f642cc2864faf0ab611
Amber <ambermcknight54@yahoo.com>
2026-06-29 09:53:26 -0500

    added import with function, export default debugger about

--- lesson-05-react-state/level-08-mount-effect/mount-effect/components/main.jsx
+++ lesson-05-react-state/level-08-mount-effect/mount-effect/components/main.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-export function Main() {
+function Main() {
+  const [didMount, setDidMount] = useState(false);
+
+  // Execution pauses here during rendering to let you see the initial state
+  debugger;
+
+  useEffect(componentDidMount, []);
+
   return (
     <main>
-      {/* 15. A descriptive paragraph detailing how to structure a semantic template */}
+      <p>{String(didMount)}</p>
       <p>
-        When you make a Semantic Template, you throw away that extra box!
-        Instead, you attach your website block.
+        The useEffect function runs code at specific times. By passing an empty
+        array at the end, we tell React to run our function exactly once, right
+        after the component first appears on the screen.
       </p>
     </main>
   );
 }
+
+function componentDidMount() {
+  // Execution pauses here after the component mounts to the screen
+  debugger;
+
+  setDidMount(true);
+}
+
+export default Main;
