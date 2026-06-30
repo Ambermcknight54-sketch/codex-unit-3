b68eddaa8eb77cce8f5af1a03ee16c764677344c
Amber <ambermcknight54@yahoo.com>
2026-06-30 09:38:27 -0500

    example

--- lesson-02-npm/level-03-named-imports/messages.js
+++ lesson-02-npm/level-03-named-imports/messages.js
@@ -2,4 +2,4 @@ export const message1 =
   " export default allow you to name the variable and export default a variable";
 export const message2 =
   "imports can have any variable name you want. It doesn't have to match";
-(" his how to do an import { message2 } from './messages.js;'");
+(" ex. import { message2 } from './messages.js;'");
