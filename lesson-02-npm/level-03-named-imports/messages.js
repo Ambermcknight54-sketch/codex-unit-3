9e6d7717d4a55ab7a365fbdc47ab22d90c27fbe0
Amber <ambermcknight54@yahoo.com>
2026-06-30 09:37:00 -0500

    added a defiinition

--- lesson-02-npm/level-03-named-imports/messages.js
+++ lesson-02-npm/level-03-named-imports/messages.js
@@ -1,3 +1,5 @@
-export const message1 = " export default allow to name and display a variable";
+export const message1 =
+  " export default allow you to name the variable and export default a variable";
 export const message2 =
-  " this how to do an import { message2 } from './messages.js;'";
+  "imports can have any variable name you want. It doesn't have to match";
+(" his how to do an import { message2 } from './messages.js;'");
