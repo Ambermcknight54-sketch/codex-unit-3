aa4f02589c64603b00f4e149cc1abd01ce5bef85
Amber <ambermcknight54@yahoo.com>
2026-06-30 08:45:14 -0500

    made numbers 1-10

--- lesson-01-nodejs/level-07-array-traversal/script.js
+++ lesson-01-nodejs/level-07-array-traversal/script.js
@@ -1,4 +1,4 @@
-const numbers = ["11", "22", "33", "44", "55", "66", "77", "88", "99", "109"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 for (let index = 0; index < numbers.length; index++) {
   const item = numbers[index];
   const itemNumber = index + 1;
