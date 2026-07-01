bfc94b0a7bd1c456b7d15fb42f9d9e40eb7d8b84
Amber <ambermcknight54@yahoo.com>
2026-07-01 10:43:31 -0500

    added data.js and code

---
+++ lesson-06-react-data/level-01-render-arrays/react-template/src/data.js
@@ -0,0 +1,30 @@
+// 9. Export default an array of 5 objects
+const imageData = [
+  {
+    src: "https://picsum.photos/id/10/200/300",
+    caption:
+      "A beautiful view of a peaceful mountain lake surrounded by trees.",
+  },
+  {
+    src: "https://picsum.photos/id/20/200/300",
+    caption:
+      "A clean and organized office workspace with a laptop and notebook.",
+  },
+  {
+    src: "https://picsum.photos/id/29/200/300",
+    caption:
+      "A scenic look at rugged mountains stretching out under a clear blue sky.",
+  },
+  {
+    src: "https://picsum.photos/id/42/200/300",
+    caption:
+      "A close-up shot of an old-school analog table clock standing outdoors.",
+  },
+  {
+    src: "https://picsum.photos/id/60/200/300",
+    caption:
+      "A large desktop computer monitor sitting on a desk showing colorful graphics.",
+  },
+];
+
+export default imageData;
