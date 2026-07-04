ce9efb6f4e8cb77210061d9ae10c28eb9711fd03
Amber <ambermcknight54@yahoo.com>
2026-07-04 15:47:21 -0500

    fixed code added own pic and added styles

--- lesson-03-react/level-09-react-images/react-images/src/App.jsx
+++ lesson-03-react/level-09-react-images/react-images/src/App.jsx
@@ -1,9 +1,9 @@
-import pic1 from "./assets/pic1.jpg";
-import pic2 from "./assets/pic2.jpg";
-import pic3 from "./assets/pic3.jpg";
+import pic1 from "./assets/emojiCode.jpg";
+import pic2 from "./assets/madEmoji.jpg";
+import pic3 from "./assets/sademoji.jpg";
 import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <main>
       <h1>React Images</h1>
@@ -13,10 +13,12 @@ export default function App() {
         variable name. Then, you pass that variable inside curly braces directly
         into the image tag's source attribute.
       </p>
-      {/* Wrapped the variables in curly braces here */}
-      <img src={pic1} alt="First gallery item" />
-      <img src={pic2} alt="Second gallery item" />
-      <img src={pic3} alt="Third gallery item" />
+      <div id="images">
+        <img src={pic1} />
+        <img src={pic2} />
+        <img src={pic3} />
+      </div>
     </main>
   );
 }
+export default App;
