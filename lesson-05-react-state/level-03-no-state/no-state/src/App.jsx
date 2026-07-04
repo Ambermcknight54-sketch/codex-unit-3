2a493752a63a16e221f927515fa9e52a0af754ab
Amber <ambermcknight54@yahoo.com>
2026-07-04 16:45:14 -0500

    deleted pTag

--- lesson-05-react-state/level-03-no-state/no-state/src/App.jsx
+++ lesson-05-react-state/level-03-no-state/no-state/src/App.jsx
@@ -9,13 +9,7 @@ function App() {
   return (
     <main>
       <h1>Stateless Variable </h1>
-      <p>
-        To make a React template, run the Vite command in your terminal to
-        create the project. Go into the new folder and run npm install to get
-        the files ready. Then, delete the starter code and extra CSS so you have
-        a clean folder for your next project. designing your own layouts right
-        away.
-      </p>
+      <p></p>
     </main>
   );
 }
