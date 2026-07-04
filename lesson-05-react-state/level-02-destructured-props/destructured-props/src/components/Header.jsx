529aa8507187bd20a5f46d52159735cbc3f45d7b
Amber <ambermcknight54@yahoo.com>
2026-07-04 16:41:29 -0500

    changed to export function

--- lesson-05-react-state/level-02-destructured-props/destructured-props/src/components/Header.jsx
+++ lesson-05-react-state/level-02-destructured-props/destructured-props/src/components/Header.jsx
@@ -1,14 +1,11 @@
 // 10 & 14. Destructured the title property directly in the parameter block
-function Header({ title }) {
+export function Header({ title }) {
   // 11. Debugger statement to inspect variables during execution
   // debugger;
-
   return (
     <header>
       {/* 16. Rendered the destructured title variable */}
-      <h1>{title}</h1>
+      <h1> {title} </h1>
     </header>
   );
 }
-
-export default Header;
