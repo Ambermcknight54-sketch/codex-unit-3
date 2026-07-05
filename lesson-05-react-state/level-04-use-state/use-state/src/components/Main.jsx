9c6846132685d0ff5be01637c4451f7284bbc641
Amber <ambermcknight54@yahoo.com>
2026-07-04 23:53:26 -0500

    rewriting code was gone

--- lesson-05-react-state/level-04-use-state/use-state/src/components/Main.jsx
+++ lesson-05-react-state/level-04-use-state/use-state/src/components/Main.jsx
@@ -1,26 +1,20 @@
 import { useState } from "react";
 
-function Main() {
-  const results = useState(0);
-
-  // Execution pauses here during rendering to let you inspect the 2-item array
-  
-
-  const count = results[0];
-  const setCount = results[1];
-
-  
-  
+export function Main() {
+  const [count, setCount] = useState(0);
   function handleClick(event) {
     event.preventDefault();
-    setCount(count + 1
+    setCount(count + 1);
   }
-
   return (
     <main>
-      <button onClick={handleClick}>Click counter</button>
       <p>{count}</p>
-      <p> "useState is like a label and a button. The count variable is the label that shows a number. The setCount function is the button that changes that number. Use array brackets on the left side. 'Event.preventDefault' stops the page from reloading";
+      <button onClick={handleClick}>Click counter</button>
+      <p>
+        useState is like a label and a button. The count variable is the label
+        that shows a number. The setCount function is the button that changes
+        that number. Use array brackets on the left side. 'Event.preventDefault'
+        stops the page from reloading";
       </p>
     </main>
   );
