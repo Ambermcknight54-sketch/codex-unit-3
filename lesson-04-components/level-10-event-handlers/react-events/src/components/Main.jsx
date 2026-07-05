7cdf90b6fc82c90525ea70dd88db5fc1b4ff1fe0
Amber <ambermcknight54@yahoo.com>
2026-07-05 08:19:45 -0500

    button not working ow addig changes

--- lesson-04-components/level-10-event-handlers/react-events/src/components/Main.jsx
+++ lesson-04-components/level-10-event-handlers/react-events/src/components/Main.jsx
@@ -1,4 +1,4 @@
-export function Main() {
+export default function Main() {
   return (
     <main>
       <button onClick={handleClick}>Click event</button>
@@ -6,9 +6,37 @@ export function Main() {
   );
   function handleClick(event) {
     event.preventDefault();
-    // debugger;
     console.log(
       "To handle click events in React, you attach an onClick attribute directly to an HTML element like a button and pass it the name of a callback function. That function automatically receives an event object which you can use to control or prevent standard browser behaviors.",
     );
   }
 }
+
+// export function Main() {
+//   function handleClick(event) {
+//     event.preventDefault();
+//     console.log(
+//       "To handle click events in React, you attach an onClick attribute directly to an HTML element like a button and pass it the name of a callback function. That function automatically receives an event object which you can use to control or prevent standard browser behaviors.",
+//     );
+//   }
+//   return (
+//     <main>
+//       <button onClick={handleClick}>Click event</button>
+//     </main>
+//   );
+// }
+
+// export function Main() {
+//   return (
+//     <main>
+//       <button onClick={handleClick}>Click event</button>
+//     </main>
+//   );
+//   function handleClick(event) {
+//     event.preventDefault();
+//     // debugger;
+//     console.log(
+//       "To handle click events in React, you attach an onClick attribute directly to an HTML element like a button and pass it the name of a callback function. That function automatically receives an event object which you can use to control or prevent standard browser behaviors.",
+//     );
+//   }
+// }
