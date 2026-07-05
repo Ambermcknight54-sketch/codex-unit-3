76b6b901a15b6f9d65befd1e1227cffe2611bb11
Amber <ambermcknight54@yahoo.com>
2026-07-05 00:22:02 -0500

    rearranged code

--- lesson-05-react-state/level-04-use-state/use-state/src/components/Main.jsx
+++ lesson-05-react-state/level-04-use-state/use-state/src/components/Main.jsx
@@ -2,22 +2,20 @@ import { useState } from "react";
 
 export function Main() {
   const [count, setCount] = useState(0);
-  function handleClick(event) {
-    event.preventDefault();
-    setCount(count + 1);
-  }
   return (
     <main>
       <p>{count}</p>
       <button onClick={handleClick}>Click counter</button>
       <p>
-        useState is like a label and a button. The count variable is the label
-        that shows a number. The setCount function is the button that changes
-        that number. Use array brackets on the left side. 'Event.preventDefault'
-        stops the page from reloading";
+        A normal functions forget everything once they finish running. A
+        stateful variable is special because it acts like a memory chip—it lets
+        your component remember information even after it runs.
       </p>
     </main>
   );
-}
 
-export default Main;
+  function handleClick(event) {
+    event.preventDefault();
+    setCount(count + 1);
+  }
+}
--- lesson-05-react-state/level-06-destructured-state/use-state-2/src/components/Main.jsx
+++ lesson-05-react-state/level-06-destructured-state/use-state-2/src/components/Main.jsx
@@ -4,11 +4,14 @@ export function Main() {
   const [count, setCount] = useState(0);
   return (
     <main>
-      <p>{count}</p>
       <button onClick={handleClick}>Click counter</button>
+      <p>{count}</p>
       <p>
-        Using useState with array destructuring lets you unpack the state
-        variable and its setter function onto a single line of code.
+        In React, components are functions, but normal functions forget their
+        data after they run. A stateful variable acts like a memory chip that
+        preserves its value between function calls. When you click the button,
+        the setter function updates this value, which tells React to clear the
+        old view and re-render the page with the brand new count.
       </p>
     </main>
   );
@@ -18,3 +21,24 @@ export function Main() {
     setCount(count + 1);
   }
 }
+
+// import { useState } from "react";
+
+// export function Main() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <main>
+//       <p>{count}</p>
+//       <button onClick={handleClick}>Click counter</button>
+//       <p>
+//         Using useState with array destructuring lets you unpack the state
+//         variable and its setter function onto a single line of code.
+//       </p>
+//     </main>
+//   );
+
+//   function handleClick(event) {
+//     event.preventDefault();
+//     setCount(count + 1);
+//   }
+// }
