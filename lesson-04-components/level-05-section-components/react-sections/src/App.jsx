b370d753fe64f64ece8444d5968fd95c42755230
Amber <ambermcknight54@yahoo.com>
2026-07-05 13:55:56 -0500

    added components to file path

--- lesson-04-components/level-05-section-components/react-sections/src/App.jsx
+++ lesson-04-components/level-05-section-components/react-sections/src/App.jsx
@@ -4,9 +4,9 @@
 // import heroImg from './assets/hero.png'
 //
 import "./App.css";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
