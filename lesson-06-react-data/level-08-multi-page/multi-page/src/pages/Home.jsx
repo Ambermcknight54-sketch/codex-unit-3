e99dc5b13a95f003a72f39b868a88593cd2ff1d2
Amber <ambermcknight54@yahoo.com>
2026-07-07 16:59:58 -0500

    added code to home

---
+++ lesson-06-react-data/level-08-multi-page/multi-page/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>Home Page</h2>
+      <p>
+        To create a multi-page website in React, we use the react-router
+        package. It allows us to define paths using BrowserRouter, Routes, and
+        Route components to switch views without reloading the browser.
+      </p>
+    </main>
+  );
+}
