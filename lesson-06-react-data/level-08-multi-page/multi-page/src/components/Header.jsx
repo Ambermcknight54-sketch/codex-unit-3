85ba1c97389b50cc233b205983aee75f71992fbb
Amber <ambermcknight54@yahoo.com>
2026-07-07 17:17:37 -0500

    added nav to Header

--- lesson-06-react-data/level-08-multi-page/multi-page/src/components/Header.jsx
+++ lesson-06-react-data/level-08-multi-page/multi-page/src/components/Header.jsx
@@ -2,6 +2,9 @@ export default function Header() {
   return (
     <header>
       <h1>Create a Multi-Page Website with React</h1>
+      <nav>
+        <a href="/">Home</a> | <a href="/about">About</a>
+      </nav>
     </header>
   );
 }
