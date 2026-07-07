809a1f657a3dbdaa8f50b47c401621397b04a600
Amber <ambermcknight54@yahoo.com>
2026-07-07 17:10:55 -0500

    added router package added footer

--- lesson-06-react-data/level-08-multi-page/multi-page/src/App.jsx
+++ lesson-06-react-data/level-08-multi-page/multi-page/src/App.jsx
@@ -1,7 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "./assets/vite.svg";
-// import heroImg from "./assets/hero.png";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Header from "./components/Header";
@@ -15,7 +11,6 @@ export default function App() {
       <Header />
 
       <Routes>
-        <br /> {/* Optional layout spacing */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
---
+++ lesson-06-react-data/level-08-multi-page/multi-page/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p> Amber McKnight 2026 My Multi-Page Website</p>
+    </footer>
+  );
+}
