dc93212eecc86211fe22bf13d435bdf5161f2203
Amber <ambermcknight54@yahoo.com>
2026-07-07 19:18:37 -0500

    added template and header

--- lesson-06-react-data/level-08-multi-page/multi-page/src/components/Header.jsx
+++ lesson-06-react-data/level-08-multi-page/multi-page/src/components/Header.jsx
@@ -2,9 +2,6 @@ export default function Header() {
   return (
     <header>
       <h1>Create a Multi-Page Website with React</h1>
-      <nav>
-        <a href="/">Home</a> | <a href="/about">About</a>
-      </nav>
     </header>
   );
 }
