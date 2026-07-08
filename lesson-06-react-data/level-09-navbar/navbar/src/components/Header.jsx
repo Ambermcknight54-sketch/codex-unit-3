35b985001f22d0da59907ee9c2f0feb780a2a948
Amber <ambermcknight54@yahoo.com>
2026-07-07 19:32:36 -0500

    deleted nav

--- lesson-06-react-data/level-09-navbar/navbar/src/components/Header.jsx
+++ lesson-06-react-data/level-09-navbar/navbar/src/components/Header.jsx
@@ -2,9 +2,6 @@ export default function Header() {
   return (
     <header>
       <h1>TW Elements Navbar in React</h1>
-      <nav>
-        <a href="/">Home</a> | <a href="/about">About</a>
-      </nav>
     </header>
   );
 }
