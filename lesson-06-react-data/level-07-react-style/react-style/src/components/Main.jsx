b3552da04d790e80699131c98a4f0b4827dd7e69
Amber <ambermcknight54@yahoo.com>
2026-07-09 21:19:50 -0500

    working on this

---
+++ lesson-06-react-data/level-07-react-style/react-style/src/components/Main.jsx
@@ -0,0 +1,12 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>React Style</h2>
+      <p>
+        To create a multi-page website in React, we use the react-router
+        package. It allows us to define paths using BrowserRouter, Routes, and
+        Route components to switch views without reloading the browser.
+      </p>
+    </main>
+  );
+}
