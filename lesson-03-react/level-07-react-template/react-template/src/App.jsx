062eb65ef735c703d5670dffce046b32b46e464f
Amber <ambermcknight54@yahoo.com>
2026-07-11 11:51:16 -0500

    added Caps to Header main and Footer

--- lesson-03-react/level-07-react-template/react-template/src/App.jsx
+++ lesson-03-react/level-07-react-template/react-template/src/App.jsx
@@ -2,6 +2,9 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
+// import Header from "./components/Header";
+// import Main from "./components/Header";
+// import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
