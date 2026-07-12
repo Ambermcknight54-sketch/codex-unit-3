bba29aec474ca3058a039bcad40adcd97fcb0909
Amber <ambermcknight54@yahoo.com>
2026-07-11 19:38:13 -0500

    removed curly braces from home in App.jsx

--- lesson-06-react-data/level-08-multi-page/multi-page/src/App.jsx
+++ lesson-06-react-data/level-08-multi-page/multi-page/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { About } from "./pages/About";
 
