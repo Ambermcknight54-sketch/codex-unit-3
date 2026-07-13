7ad1cfb851403cd6b9cd506b64cdac740ceb0e78
Amber <ambermcknight54@yahoo.com>
2026-07-13 17:03:48 -0500

    linked App.css and syled

--- lesson-03-react/level-07-react-template/react-template/src/App.jsx
+++ lesson-03-react/level-07-react-template/react-template/src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
-// src/App.jsx
+import "./App.css";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
