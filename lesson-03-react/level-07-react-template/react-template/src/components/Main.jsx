ad29275ed8ec56f13f9217e458c880e3c6358ba9
Amber <ambermcknight54@yahoo.com>
2026-07-13 17:16:23 -0500

    added an h2

--- lesson-03-react/level-07-react-template/react-template/src/components/Main.jsx
+++ lesson-03-react/level-07-react-template/react-template/src/components/Main.jsx
@@ -1,6 +1,7 @@
 export function Main() {
   return (
     <main>
+      <h2> What is a React Template</h2>
       <p>
         A React template is a starter codebase designed to jumpstart the
         development of a web application. Instead of configuring everything from
