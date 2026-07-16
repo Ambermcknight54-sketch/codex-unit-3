1c3c9b846472009f991413bd6c908a943bb8b2f0
Amber <ambermcknight54@yahoo.com>
2026-07-15 21:46:05 -0400

    level 1 complete

--- lesson-07-database/level-01-api-key/api-key/src/App.jsx
+++ lesson-07-database/level-01-api-key/api-key/src/App.jsx
@@ -3,8 +3,9 @@
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
 import "./App.css";
+
+import Main from "./components/Main";
 import { Header } from "./components/Header";
-import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 
 function App() {
--- lesson-07-database/level-01-api-key/api-key/src/components/Main.jsx
+++ lesson-07-database/level-01-api-key/api-key/src/components/Main.jsx
@@ -1,13 +1,64 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/set-state-in-effect */
+// eslint-disable-next-line react-hooks/set-state-in-effect
+import { useState, useEffect } from "react";
+
 export function Main() {
+  // Creating state using array destructuring
+  const [apiKey, setApiKey] = useState("");
+  // Track the mount phase with useEffect
+  useEffect(function componentDidMount() {
+    // Get the apiKey from sessionStorage
+    const savedKey = sessionStorage.getItem("apiKey");
+
+    // If a saved key exists, restore it to state
+    if (savedKey) {
+      setApiKey(savedKey);
+    }
+  }, []); // Empty array of dependencies so it only runs on mount
+  //  function for handling form submission
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitted API Key:", apiKey);
+  }
+
+  // function for updating state on input change
+  function handleInputChange(event) {
+    setApiKey(event.target.value);
+  }
+
   return (
     <main>
-      <h2> What is a React Template</h2>
-      <p>
-        A React template is a starter codebase designed to jumpstart the
-        development of a web application. Instead of configuring everything from
-        scratch, it provides a pre-structured foundation with essential files,
-        dependencies, and layout pieces already set up.
-      </p>
+      {/* Setup form tag with labels, input, name attribute, and a submit button */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          API Key:
+          <input
+            type="password"
+            name="apiKey"
+            value={apiKey}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit"> Submit </button>
+      </form>
+
+      {/* Output tag below the form rendering the apiKey */}
+      <output>{apiKey}</output>
     </main>
   );
 }
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const value = form.apiKey.value;
+
+  sessionStorage.setItem("apiKey", value);
+  setApiKey(value);
+  console.log("Saved API Key:", value);
+}
+
+export default Main;
