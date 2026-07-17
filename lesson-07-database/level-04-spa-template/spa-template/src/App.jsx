04f7d98f83e0aa77a1f8feb75cc30be61fcfafdd
Amber <ambermcknight54@yahoo.com>
2026-07-16 23:54:23 -0400

    deleted Main and Footer from App.jsx and added a header to Header to view the page

--- lesson-07-database/level-04-spa-template/spa-template/src/App.jsx
+++ lesson-07-database/level-04-spa-template/spa-template/src/App.jsx
@@ -4,15 +4,11 @@
 // import heroImg from "./assets/hero.png";
 import "./App.css";
 import { Header } from "./components/Header";
-import { Main } from "./components/Main";
-import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <main>
       <Header />
-      <Main />
-      <Footer />
     </main>
   );
 }
