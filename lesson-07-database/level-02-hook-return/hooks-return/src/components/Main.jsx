13a8593c3322d21f13f08c889678b1f14cc6d2a9
Amber <ambermcknight54@yahoo.com>
2026-07-16 21:31:20 -0400

    added code to Main per instructions

--- lesson-07-database/level-02-hook-return/hooks-return/src/components/Main.jsx
+++ lesson-07-database/level-02-hook-return/hooks-return/src/components/Main.jsx
@@ -1,13 +1,40 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/set-state-in-effect */
+// eslint-disable-next-line react-hooks/set-state-in-effect
+import React from "react";
+import { useSecret } from "../hooks/useSecret";
+
 export function Main() {
+  // Pass the target input name / sessionStorage key as an argument
+  const [secretValue, handleSubmit] = useSecret("mySecretToken");
+
   return (
     <main>
-      <h2> What is a React Template</h2>
       <p>
-        A React template is a starter codebase designed to jumpstart the
-        development of a web application. Instead of configuring everything from
-        scratch, it provides a pre-structured foundation with essential files,
-        dependencies, and layout pieces already set up.
+        To save a secret value, the form's onSubmit handler intercepts the
+        submission event, prevents the default browser refresh, retrieves the
+        input's value, and saves it to sessionStorage while also updating our
+        hook's state. To restore the secret when the component mounts, we use
+        the useEffect hook inside our custom useSecret hook.
       </p>
+
+      {/* Setup form tag with labels, input, name attribute, and a submit button */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Secret Key:
+          <input
+            type="password"
+            name="mySecretToken" // Must match the argument passed to useSecret
+            defaultValue={secretValue} // Allows user typing while holding stored state
+            required
+          />
+        </label>
+        <br />
+        <button type="submit"> Submit </button>
+      </form>
+
+      {/* Output tag below the form rendering the secretValue */}
+      <output>{secretValue}</output>
     </main>
   );
 }
