4c2a7d442d6f8f8b23943ee77955bf8ab5d22fec
Amber <ambermcknight54@yahoo.com>
2026-07-17 11:35:11 -0400

    added template

---
+++ lesson-07-database/level-04-spa-template/react-template/src/components/Main.jsx
@@ -0,0 +1,13 @@
+export function Main() {
+  return (
+    <main>
+      <h2> What is a React Template</h2>
+      <p>
+        A React template is a starter codebase designed to jumpstart the
+        development of a web application. Instead of configuring everything from
+        scratch, it provides a pre-structured foundation with essential files,
+        dependencies, and layout pieces already set up.
+      </p>
+    </main>
+  );
+}
--- lesson-07-database/level-04-spa-template/spa-template/src/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-export function About() {
-  return (
-    <main>
-      <h1>About</h1>
-      <p> This page has information about this website</p>
-    </main>
-  );
-}
