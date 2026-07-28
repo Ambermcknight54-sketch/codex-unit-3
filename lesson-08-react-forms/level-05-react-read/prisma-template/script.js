587a242570b7c2066f00d845cb4047a5bea0e726
Amber <ambermcknight54@yahoo.com>
2026-07-28 00:57:35 -0500

    copied and paste client

--- lesson-08-react-forms/level-05-react-read/prisma-template/script.js
+++ lesson-08-react-forms/level-05-react-read/prisma-template/script.js
@@ -1,34 +1,7 @@
-import { PrismaClient } from "./prisma-template/generated/prisma";
+// import { PrismaClient } from "./generated/prisma/client.js";
+import { createWebClient } from "./web-client.js";
 
-const prisma = new PrismaClient();
-
+// const prisma = new PrismaClient();
+const prisma = await createWebClient({ jsonSchema: "json-schema.json" });
 const results = await prisma.products.findMany();
 console.log(results);
-// const prisma = new PrismaClient();
-// let results;
-
-// results = await prisma.users.create({
-//   data: {
-//     name: "purifier",
-//     src: "https://www.amazon.com/Airspark-Purifiers-Purifier-Cooking-Cleaner/dp/B0CGPTRQR2?th=1",
-//     price: 85.0, // Pass as a number
-//   },
-// });
-// console.log("CREATE", results);
-
-// results = await prisma.users.update({
-//   where: {
-//     id: 10,
-//   },
-//   data: {
-//     price: 13.45,
-//   },
-// });
-// console.log("UPDATE", results);
-
-// results = await prisma.users.delete({
-//   where: {
-//     id: 7,
-//   },
-// });
-// console.log("DELETE", results);
