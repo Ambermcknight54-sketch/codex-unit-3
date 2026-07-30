8682bd55af101686386c8eb722a47ec7a692596d
Amber <ambermcknight54@yahoo.com>
2026-07-29 20:55:24 -0500

    added nodules

---
+++ lesson-08-react-forms/level-07-react-delete/react-delete/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1> Use a React Form to Read Data </h1>
+    </header>
+  );
+}
---
+++ lesson-08-react-forms/level-07-react-delete/react-delete/src/hooks/useDelete.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export function useRead(prisma) {
+  const [data, setData] = useState();
+  return [data, handleSubmit];
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (prisma) {
+      const form = event.target;
+      const where = {};
+
+      const productId = form.elements.productId.value;
+      const productName = form.elements.productName.value;
+      const productPrice = form.elements.productPrice.value;
+
+      if (productId) where.id = productId;
+      if (productName) where.name = productName;
+      if (productPrice) where.price = { lte: productPrice };
+
+      const results = await prisma.products.findMany({ where });
+
+      setData(results);
+    }
+  }
+}
+
+export default useRead;
+
+// import { useState } from "react";
+
+// export function useRead(prisma) {
+//   const [data, setData] = useState();
+//   return [data, handleSubmit];
+
+//   async function handleSubmit(event) {
+//     event.preventDefault();
+//       const form = event.target;
+//       const where = {
+//         id: form.elements.productId.value,
+//       };
+//       const data = ();
+//       const productName = form.elements.productName.value;
+//       const productPrice = form.elements.productPrice.value;
+//       const productImage = form.elements.productImage.value;
+//       if (productName) data.name = productName;
+//       if (productPrice) where.src = productPrice;
+//       if (productPrice) where.price =productPrice;
+
+//       const results = await prisma.products.findMany({ where });
+//       setData([results]);
+//     }
+//   }
+
+//     name: form.elements.productName.value,
+//     src: form.elements.productImage.value,
+//     price: form.elements.productPrice.value,
+//   };
+//   await prisma.products.create({ data });
+//   const results = await prisma.products.findMany();
+//   setData(results );
