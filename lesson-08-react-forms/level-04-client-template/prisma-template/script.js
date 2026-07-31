b35a3772c06f737a408eb26b5ffad7f77c1100bc
Amber <ambermcknight54@yahoo.com>
2026-07-30 23:28:46 -0500

    finally fixed template it shall work thank you Jesus

--- lesson-08-react-forms/level-04-client-template/prisma-template/script.js
+++ lesson-08-react-forms/level-04-client-template/prisma-template/script.js
@@ -3,5 +3,9 @@ import { createWebClient } from "./web-client.js";
 
 // const prisma = new PrismaClient();
 const prisma = await createWebClient({ jsonSchema: "json-schema.json" });
-const results = await prisma.products.findMany();
+const results = await prisma.products.findMany({
+  include: {
+    reviews: true,
+  },
+});
 console.log(results);
