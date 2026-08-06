648e2cb0f7ed212a4ff502c03ee0b8072fe5f9ee
Amber <ambermcknight54@yahoo.com>
2026-08-06 06:27:40 -0500

    deletd duplicate

--- lesson-08-react-forms/level-01-react-create/script.js
+++
@@ -1,5 +0,0 @@
-import { createWebClient } from "./web-client.js";
-
-const prisma = await createWebClient({ jsonSchema: "json-schema.json" });
-const results = await prisma.products.findMany();
-console.log(results);
