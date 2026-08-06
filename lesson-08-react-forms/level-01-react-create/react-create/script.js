fa013ba825e4cae25a36978ff31fe6ffd9b2da09
Amber <ambermcknight54@yahoo.com>
2026-08-06 06:28:35 -0500

    added to level

---
+++ lesson-08-react-forms/level-01-react-create/react-create/script.js
@@ -0,0 +1,5 @@
+import { createWebClient } from "./web-client.js";
+
+const prisma = await createWebClient({ jsonSchema: "json-schema.json" });
+const results = await prisma.products.findMany();
+console.log(results);
