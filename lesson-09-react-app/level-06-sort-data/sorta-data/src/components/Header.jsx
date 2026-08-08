8057f63166210c7ccc5d1c8898eab0725a3c1b6a
Amber <ambermcknight54@yahoo.com>
2026-08-08 15:00:24 -0500

    added Navbar

--- lesson-09-react-app/level-06-sort-data/sorta-data/src/components/Header.jsx
+++ lesson-09-react-app/level-06-sort-data/sorta-data/src/components/Header.jsx
@@ -1,6 +1,9 @@
+import { Navbar } from "../components/Navbar";
+
 export function Header() {
   return (
     <header>
+      <Navbar />
       <h1> Real-time Sort with Controlled Inputs </h1>
     </header>
   );
